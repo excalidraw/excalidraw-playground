b3a80b42acbca886af6ccfe46459c2e52f780f50
Panayiotis Lipiridis <lipiridis@gmail.com>
2020-03-26 16:31:28 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# excalidraw-playground
+# Excalidraw Playground
+
+- [Generate column graphs](graph)
+  - [Example 1](https://excalidraw.com/#json=5157399421779968,I2o7TYkeX758PafUpyrMnw)
+  - [Example 2](https://excalidraw.com/#json=6307444468744192,jbWIElaKNsWJ7djZviaKIg)
---
+++ graph/graph.py
@@ -0,0 +1,121 @@
+import json
+import random
+
+SOURCE = "data2.csv"
+
+COLORS = [
+    "#ced4da",
+    "#868e96",
+    "#fa5252",
+    "#e64980",
+    "#be4bdb",
+    "#7950f2",
+    "#4c6ef5",
+    "#228be6",
+    "#15aabf",
+    "#12b886",
+    "#40c057",
+    "#82c91e",
+    "#fab005",
+    "#fd7e14",
+]
+
+data = []
+
+title_x = "X Axis"
+title_y = "Y Axis"
+
+MAX_HEIGHT = 400
+MAX_WIDTH = 600
+
+excalidraw = {
+    "version": 1,
+    "type": "excalidraw",
+    "elements": [],
+    "appState": {"viewBackgroundColor": "#ffffff"},
+}
+
+
+def new_element(element_type):
+    return {
+        "type": element_type,
+        "opacity": 100,
+        "seed": random.randint(1000000, 100000000),
+        "strokeColor": "#000000",
+        "fillStyle": "hachure",
+        "strokeWidth": 1,
+        "roughness": 1,
+        "opacity": 100,
+    }
+
+
+def new_arrow(x, y, width, height):
+    arrow = new_element("arrow")
+    arrow["x"] = x
+    arrow["y"] = y
+    arrow["width"] = width
+    arrow["height"] = height
+    arrow["points"] = [
+        [x, y],
+        [width, height],
+    ]
+    return arrow
+
+
+def new_text(x, y, width, text):
+    element = new_element("text")
+    element["x"] = x
+    element["y"] = y
+    element["width"] = width
+    element["height"] = 20
+    element["text"] = text
+    element["font"] = "16px Virgil"
+    element["baseline"] = 14
+    return element
+
+
+def get_random_color():
+    return random.choice(COLORS)
+
+
+with open(SOURCE, "r") as file:
+    header = False
+    for line in file.readlines():
+        if not header:
+            title_x, title_y = line.split(",")
+            header = True
+        else:
+            key, value = line.split(",")
+            data.append([key, float(value)])
+
+max_y = 0
+max_x = len(data)
+
+for key, value in data:
+    max_y = max(max_y, value)
+
+# Draw charts
+width = MAX_WIDTH / max_x
+gap = width / 8
+x = 0
+color = get_random_color()
+
+for key, value in data:
+    element = new_element("rectangle")
+    element["x"] = x * width + gap
+    element["y"] = -gap
+    element["width"] = width - gap
+    element["height"] = -value * (MAX_HEIGHT / max_y)
+    element["backgroundColor"] = color
+
+    excalidraw["elements"].append(element)
+    excalidraw["elements"].append(new_text(x * width + gap, gap, width, key))
+    x += 1
+
+
+# Draw lines
+excalidraw["elements"].append(new_arrow(0, 0, 0, -MAX_HEIGHT - width))
+excalidraw["elements"].append(new_arrow(0, 0, MAX_WIDTH + width, 0))
+
+with open("graph.excalidraw", "w") as excalidraw_file:
+    json.dump(excalidraw, excalidraw_file, indent=2)
