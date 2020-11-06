f506553948f2664dfaccae669bcc4a694ca474ca
Panayiotis Lipiridis <lipiridis@gmail.com>
2020-11-07 01:35:40 +0200

    Poligons

--- py-excalidraw/polygons.py
+++ py-excalidraw/polygons.py
@@ -1,4 +1,5 @@
-# https://excalidraw.com/?addLibrary=https://gist.githubusercontent.com/lipis/c148134151ac57b7f5df62cba69a4ee4/raw/b4b9bfa27c7399ebb6e4390fdfae2162ff768b39/ngons.excalidrawlib
+# https://gist.github.com/lipis/c148134151ac57b7f5df62cba69a4ee4
+# https://excalidraw.com/?addLibrary=https://gist.githubusercontent.com/lipis/c148134151ac57b7f5df62cba69a4ee4/raw/b4b9bfa27c7399ebb6e4390fdfae2162ff768b39/polygons.excalidrawlib
 # https://excalidraw.com/#json=5636464553492480,cBE2J8KaCrIGGucL7UpGbg
 
 import excalidraw
@@ -9,16 +10,16 @@ scene = excalidraw.Excalidraw()
 radius = 80
 initial_angle = math.pi / 2 + math.pi
 
-ngons = [3, 4, 5, 6, 7, 8, 10, 12]
+polygons = [3, 4, 5, 6, 7, 8, 10, 12]
 
-for index, ngon in enumerate(ngons):
+for index, polygon in enumerate(polygons):
     points = []
-    for point in range(ngon + 1):
+    for point in range(polygon + 1):
         # source: https://stackoverflow.com/a/7198179/8418
         points.append(
             [
-                radius * math.cos(2 * math.pi * point / ngon + initial_angle),
-                radius * math.sin(2 * math.pi * point / ngon + initial_angle),
+                radius * math.cos(2 * math.pi * point / polygon + initial_angle),
+                radius * math.sin(2 * math.pi * point / polygon + initial_angle),
             ]
         )
     scene.add_line(
